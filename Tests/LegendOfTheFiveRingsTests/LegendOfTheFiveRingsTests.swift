1fabcb1ef7085752b283212178eab5356918c134
Tharak <tharakzn@gmail.com>
2020-11-28 12:48:46 -0300

    remove test resources

--- Tests/LegendOfTheFiveRingsTests/LegendOfTheFiveRingsTests.swift
+++ Tests/LegendOfTheFiveRingsTests/LegendOfTheFiveRingsTests.swift
@@ -11,24 +11,6 @@ final class LegendOfTheFiveRingsTests: XCTestCase {
 
     static var allTests = [
         ("testExample", testExample),
-        ("testBookContent", testBookContent),
     ]
-    
-    func testBookContent() {
-        let book = Book()
-        assert(!book.advatages.isEmpty)
-        assert(!book.ancestors.isEmpty)
-        assert(!book.armors.isEmpty)
-        assert(!book.clans.isEmpty)
-        assert(!book.disadvatages.isEmpty)
-        assert(!book.families.isEmpty)
-        assert(!book.katas.isEmpty)
-        assert(!book.kihos.isEmpty)
-        assert(!book.schools.isEmpty)
-        assert(!book.shadowlandsPowers.isEmpty)
-        assert(!book.skills.isEmpty)
-        assert(!book.spells.isEmpty)
-        assert(!book.tattoos.isEmpty)
-        assert(!book.weapons.isEmpty)
-    }
+
 }
